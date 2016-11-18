d9232fd0487ff99188628b9d0e3ab732cf09bf0c
Adam <avandurmen0001@kctcs.edu>
2016-11-18 01:36:40 -0600

    add question to quiz is functional/commented code

--- FInal Project/FInal Project/frmAddQuestion.Designer.vb
+++ FInal Project/FInal Project/frmAddQuestion.Designer.vb
@@ -22,31 +22,33 @@ Partial Class frmAddQuestion
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
         Me.lblQuestion = New System.Windows.Forms.Label()
         Me.lblNewAnswer = New System.Windows.Forms.Label()
         Me.txtNewQuestion = New System.Windows.Forms.RichTextBox()
         Me.txtNewAnswer = New System.Windows.Forms.RichTextBox()
+        Me.txtCorrectAnswer = New System.Windows.Forms.RichTextBox()
+        Me.lblCorrectAnswer = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnSubmit
         '
-        Me.Button1.Location = New System.Drawing.Point(212, 227)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(212, 66)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnSubmit.Location = New System.Drawing.Point(212, 227)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(212, 66)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnCancel
         '
-        Me.Button2.Location = New System.Drawing.Point(713, 227)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(212, 66)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnCancel.Location = New System.Drawing.Point(713, 227)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(212, 66)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
         '
         'lblQuestion
         '
@@ -70,7 +72,7 @@ Partial Class frmAddQuestion
         '
         Me.txtNewQuestion.Location = New System.Drawing.Point(12, 37)
         Me.txtNewQuestion.Name = "txtNewQuestion"
-        Me.txtNewQuestion.Size = New System.Drawing.Size(571, 96)
+        Me.txtNewQuestion.Size = New System.Drawing.Size(571, 171)
         Me.txtNewQuestion.TabIndex = 4
         Me.txtNewQuestion.Text = ""
         '
@@ -82,27 +84,48 @@ Partial Class frmAddQuestion
         Me.txtNewAnswer.TabIndex = 5
         Me.txtNewAnswer.Text = ""
         '
+        'txtCorrectAnswer
+        '
+        Me.txtCorrectAnswer.Location = New System.Drawing.Point(604, 151)
+        Me.txtCorrectAnswer.Name = "txtCorrectAnswer"
+        Me.txtCorrectAnswer.Size = New System.Drawing.Size(571, 57)
+        Me.txtCorrectAnswer.TabIndex = 6
+        Me.txtCorrectAnswer.Text = ""
+        '
+        'lblCorrectAnswer
+        '
+        Me.lblCorrectAnswer.AutoSize = True
+        Me.lblCorrectAnswer.Location = New System.Drawing.Point(601, 135)
+        Me.lblCorrectAnswer.Name = "lblCorrectAnswer"
+        Me.lblCorrectAnswer.Size = New System.Drawing.Size(162, 13)
+        Me.lblCorrectAnswer.TabIndex = 7
+        Me.lblCorrectAnswer.Text = "Please type the corrrect answer: "
+        '
         'frmAddQuestion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1200, 317)
+        Me.Controls.Add(Me.lblCorrectAnswer)
+        Me.Controls.Add(Me.txtCorrectAnswer)
         Me.Controls.Add(Me.txtNewAnswer)
         Me.Controls.Add(Me.txtNewQuestion)
         Me.Controls.Add(Me.lblNewAnswer)
         Me.Controls.Add(Me.lblQuestion)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSubmit)
         Me.Name = "frmAddQuestion"
         Me.Text = "frmAddQuestion"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
     Friend WithEvents lblQuestion As System.Windows.Forms.Label
     Friend WithEvents lblNewAnswer As System.Windows.Forms.Label
     Friend WithEvents txtNewQuestion As System.Windows.Forms.RichTextBox
     Friend WithEvents txtNewAnswer As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtCorrectAnswer As System.Windows.Forms.RichTextBox
+    Friend WithEvents lblCorrectAnswer As System.Windows.Forms.Label
 End Class
--- FInal Project/FInal Project/frmAddQuestion.vb
+++ FInal Project/FInal Project/frmAddQuestion.vb
@@ -1,15 +1,35 @@
-﻿Public Class frmAddQuestion
+﻿Imports System
+Imports System.IO
+Public Class frmAddQuestion
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        'write new question, possible answers and correct answer to the respecting files into the next index
+        Try
+            Using nq As StreamWriter = File.AppendText("Questions.txt")
+                nq.WriteLine(txtNewQuestion.Text)
+            End Using
+            Using na As StreamWriter = File.AppendText("Answers.txt")
+                na.WriteLine(txtNewAnswer.Text)
+            End Using
+            Using nca As StreamWriter = File.AppendText("AnswerKey.txt")
+                nca.WriteLine(txtCorrectAnswer.Text)
+            End Using
+            MsgBox("Question Submitted Successfully")
 
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub frmAddQuestion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'reads files and populates the txtboxes with the correct next question number with respect to their index
         Dim linesQ() As String = System.IO.File.ReadAllLines("Questions.txt")
-        Dim index As String = linesQ(+1).Last
+        Dim linesA() As String = System.IO.File.ReadAllLines("Answers.txt")
+        Dim linesAk() As String = System.IO.File.ReadAllLines("AnswerKey.txt")
 
-
-        'txtNewQuestion.Text = linesQ(+1).Last
+        txtNewQuestion.Text = CStr(linesQ.Count + 1) + ", "
+        txtNewAnswer.Text = CStr(linesA.Count + 1) + ", "
+        txtCorrectAnswer.Text = CStr(linesAk.Count + 1) + ", "
 
     End Sub
 End Class
